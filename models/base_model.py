98f7e95608905fde1929ffec553c2bf50cd15d09
Dhaker Achour <achour.dhaker@gmail.com>
2023-04-14 03:30:45 -0700

    Models

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,90 @@
 #!/usr/bin/python3
-"""This script is the base model"""
-
-import uuid
+"""This is the base model class for AirBnB"""
+import models
 from datetime import datetime
-from models import storage
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+from os import environ
+import uuid
 
+Base = declarative_base()
 
+s = "HBNB_TYPE_STORAGE"
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
+    """This class will defines all common attributes/methods
+    for other classes
+    """
+    if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+        id = Column(
+            String(60),
+            unique=True,
+            nullable=False,
+            primary_key=True,
+            default=str(
+                uuid.uuid4()))
+        created_at = Column(
+            DateTime,
+            nullable=False,
+            default=datetime.utcnow())
+        updated_at = Column(
+            DateTime,
+            nullable=False,
+            default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        '''
+        Instantiation of base model class
+        '''
+        cs = "HBNB_TYPE_STORAGE"
+        if kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        elif cs not in environ.keys() or environ["HBNB_TYPE_STORAGE"] != "db":
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
-        """Returns official string representation"""
+        '''
+        returns a string
+        '''
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+    def __repr__(self):
+        '''
+        representaion
+        '''
+        return self.__str__()
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
+        '''
+        updates updated_at to current
+        '''
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        '''
+        creates dictionary of the class
+        '''
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        if "_sa_instance_state" in my_dict.keys():
+            del my_dict["_sa_instance_state"]
+            models.storage.save()
         return my_dict
+
+    def delete(self):
+        models.storage.delete(self)
+        models.storage.save()
--- models/review.py
+++ models/review.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python3
-"""This module creates a Review class"""
+"""This is the review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from os import environ
+from uuid import uuid4
 
-from models.base_model import BaseModel
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Review(BaseModel, Base):
+        """This is the class for Review
+        Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+        """
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-
-class Review(BaseModel):
-    """Class for managing review objects"""
-
-    place_id = ""
-    user_id = ""
-    text = ""
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class Review(BaseModel):
+        """This is the class for Review
+        Attributes:
+            place_id: place id
+            user_id: user id
+            text: review description
+        """
+        place_id = ""
+        user_id = ""
+        text = ""
