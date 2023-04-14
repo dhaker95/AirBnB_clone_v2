c16d0b242de9cee5274ad7f2eef2562f2d53c89b
Dhaker Achour <achour.dhaker@gmail.com>
2023-04-14 03:39:47 -0700

    Init

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-"""Initializes the package"""
+"""create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload() 
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
